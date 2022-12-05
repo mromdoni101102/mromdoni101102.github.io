84cf34353afe72f079cf6d01af3283de9c5fff3d
m. rom donii <m.romdoni101102@gmail.com>
2022-12-05 15:50:39 +0700

    first commit

---
+++ array_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array</title>
+</head>
+<body>
+    <h2>Indexed array</h2>
+
+    <?php 
+    $buah[0] = "Mangga";
+    $buah[1] = "Apel";
+    $buah[2] = "Jeruk";
+
+    echo "Buah ". $buah[0]." dan ". $buah[1] . " rasanya manis sekali";
+    ?>
+</body>
+</html>
